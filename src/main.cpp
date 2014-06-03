c7d0f2d90ccce68044666ff3c90bac83271a5996
Janis Danisevskis <werwurm@snafu.de>
2014-06-03 21:33:42 +0200

    revisited pinentry protocol implementation and connected with Plymouth interface

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,13 @@
 #include <functional>
 #include <ostream>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include "Plymouth.h"
+#include "globals.h"
+
+//static std::ofstream _log("/tmp/plymentry.log", std::ios_base::out | std::ios_base::app);
 
 // Commands Enum
 #define DEFINE_PE_CMD(name) name,
@@ -38,62 +45,186 @@ struct get_cmd_str {};
 #include "pe_commands.h"
 #undef DEFINE_PE_CMD
 
+enum pe_errors {
+    UNKNOWN_COMMAND = 0x5000113,
+    CANCELED =        0x5000063,
+    NOT_CONFIRMED =   0x5000072,
+    SERVER_FAULT =    0x5000000 | 80,
+};
 
 template <pe_command CMD>
 void handle_pe_command(const std::string & arg, std::ostream & out){
+    log << "--> " << get_cmd_str<CMD>::str() << " " << arg << std::endl;
     out << "OK\n";
 }
 
-//struct PinentryCmdHandler{
-//    virtual void handle(const std::string & arg, std::ostream & out) = 0;
-//};
-//
-//template <pe_command CMD>
-//struct TPinentryCmdHandler : public PinentryCmdHandler{
-//    virtual void handle(const std::string & arg, std::ostream & out){
-//	out << "OK\n";
-//    }
-//};
-//
-//template <>
-//struct TPinentryCmdHandler<> : public Pinentry{
-//    virtual void handle(const std::string & arg, std::ostream & out){
-//	out << "OK\n";
-//    }
-//};
-
-typedef std::map<const std::string, std::function<void(const std::string &, std::ostream &)> > handler_map_t;
-static handler_map_t pe_handler_map;
-
-// init handler map
 #define DEFINE_PE_CMD(name) \
-    pe_handler_map.insert(handler_map_t::value_type(get_cmd_str<name>::str(), handle_pe_command<name>));
-
-void init_handlers(){
-#include "pe_commands.h"
+case name: return get_cmd_str<name>::str();
+const std::string & getCmdString(const pe_command cmd){
+    static const std::string dummy("");
+    switch(cmd){
+	#include "pe_commands.h"
+    default: return dummy;
+    }
 }
 #undef DEFINE_PE_CMD
 
-void main_loop(){
-    std::string line;
-    while(!std::cin.eof()){
-	std::getline(std::cin, line);
-	if(!line.size()) continue;
-	auto pos = line.find_first_of(' ');
-	auto cmd = line.substr(0, pos);
-	auto arg = line.substr(pos + 1);
-	auto handler = pe_handler_map.find(cmd);
-	if (handler == pe_handler_map.end()){
-	    std::cout << "ERR 83886355 unknown command" << std::endl;
-	} else {
-	    handler->second(arg, std::cout);
+class PEPipeServer{
+public:
+    typedef std::function<void(const std::string &, std::ostream &)> handler_t;
+private:
+    typedef std::map<const std::string, handler_t> handler_map_t;
+    handler_map_t _handler_map;
+public:
+    #define DEFINE_PE_CMD(name) \
+    _handler_map.insert(handler_map_t::value_type(get_cmd_str<name>::str(), handle_pe_command<name>));
+    PEPipeServer(){
+	#include "pe_commands.h"
+    }
+    #undef DEFINE_PE_CMD
+    void registerHandler(const std::string & command, handler_t && handler){
+	_handler_map[command] = handler;
+    }
+    void registerHandler(const pe_command cmd, handler_t && handler){
+	registerHandler(getCmdString(cmd), std::move(handler));
+    }
+    void enterLoop (std::istream & in, std::ostream & out){
+	out << "OK Your orders please\n";
+	std::string line;
+	std::string arg;
+	while(!in.eof()){
+	    std::getline(in, line);
+	    if(!line.size()) continue;
+	    auto pos = line.find_first_of(' ');
+	    auto cmd = line.substr(0, pos);
+	    if(pos != std::string::npos) {
+		arg = line.substr(pos + 1);
+	    } else {
+		arg = "";
+	    }
+	    auto handler = _handler_map.find(cmd);
+	    if (handler == _handler_map.end()){
+		out << "ERR " << UNKNOWN_COMMAND << " unknown command" << std::endl;
+	    } else {
+		try{
+		    handler->second(arg, std::cout);
+		} catch (std::runtime_error & e){
+		    log << e.what() << std::endl;
+		    out << "ERR " << SERVER_FAULT << " server fault" << std::endl;
+		}
+	    }
 	}
     }
+};
+
+inline static jd::Plymouth::answer_t plym_check(jd::Plymouth::answer_t err){
+    if(err == jd::Plymouth::FAILED){
+	throw std::runtime_error("fail");
+    }
+    return err;
 }
 
+class PlymEntry: public PEPipeServer{
+    jd::Plymouth _ply;
+    std::string _desc;
+    std::string _prompt;
+    std::string _title;
+    std::string _ok;
+    std::string _cancel;
+public:
+    PlymEntry()
+    : _desc("")
+    , _prompt("Passphrase:")
+    , _title("")
+    , _ok("OK")
+    , _cancel("Cancel")
+    {
+	registerHandler(SETDESC,
+		[this] (const std::string & arg, std::ostream & out) -> void{
+		    _desc = arg;
+		    out << "OK\n";
+		});
+	registerHandler(SETPROMPT,
+		[this] (const std::string & arg, std::ostream & out) -> void{
+		    _prompt = arg;
+		    out << "OK\n";
+		});
+	registerHandler(SETTITLE,
+		[this] (const std::string & arg, std::ostream & out) -> void{
+		    _title = arg;
+		    out << "OK\n";
+		});
+	registerHandler(SETOK,
+		[this] (const std::string & arg, std::ostream & out) -> void{
+		    _ok = arg;
+		    out << "OK\n";
+		});
+	registerHandler(SETCANCEL,
+		[this] (const std::string & arg, std::ostream & out) -> void{
+		    _cancel = arg;
+		    out << "OK\n";
+		});
+	registerHandler(GETPIN,
+		[this] (const std::string & arg, std::ostream & out) -> void{
+		    std::string password;
+		    _ply.pause();
+		    jd::Plymouth::answer_t result = plym_check(_ply.askPassword(_prompt, password));
+		    _ply.unpause();
+		    if(result == jd::Plymouth::OK){
+			out << "D " << password << "\n";
+			out << "OK\n";
+			return;
+		    }
+		    out << "ERR " << CANCELED << " canceled\n";
+		});
+	auto message_hdl = [this] (const std::string & arg, std::ostream & out) -> void{
+	    _ply.pause();
+	    std::stringstream message;
+	    message << _desc << "\n\nPress any key to continue";
+	    jd::Plymouth::answer_t result = plym_check(_ply.displayMessage(message.str()));
+	    std::string keys("nothing assigned");
+	    result = _ply.whatchKey("", keys);
+	    _ply.displayMessage("");
+	    _ply.unpause();
+	    out << "OK\n";
+	};
+	registerHandler(MESSAGE, message_hdl);
+	registerHandler(CONFIRM,
+		[this, message_hdl] (const std::string & arg, std::ostream & out) -> void{
+		    if (arg.find("--one-button") != std::string::npos){
+			message_hdl(arg, out);
+		    } else {
+			_ply.pause();
+			std::stringstream message;
+			message << _desc << "\n\nPress y to confirm or n to cancel";
+			jd::Plymouth::answer_t result = plym_check(_ply.displayMessage(message.str()));
+			std::string keys("nothing assigned");
+			do{
+			    result = plym_check(_ply.whatchKey("", keys));
+			    if(keys == "n" || keys == "N"){
+				out << "ERR " << CANCELED << " canceled\n";
+				break;
+			    } else if(keys == "y" || keys == "Y"){
+				out << "OK\n";
+				break;
+			    }
+			} while(!result);
+			_ply.displayMessage("");
+			_ply.unpause();
+		    }
+		});
+    }
+};
+
 int main(int argc, char *args[]){
-    init_handlers();
-    main_loop();
+    log << "plymentry sais hi!" << std::endl;
+    try{
+	PlymEntry plyme;
+	plyme.enterLoop(std::cin, std::cout);
+    } catch (std::runtime_error & e){
+	log << e.what() << std::endl;
+	return 2;
+    }
     return 0;
 }
 
